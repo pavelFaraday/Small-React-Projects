4e579000323beec4d187ebe24fea0d4c27fbe8d0
pavelFaraday <devstudio.corp@gmail.com>
2022-11-04 21:03:51 +0400

    Upload 1.use-state

--- use-state/src/App.js
+++ use-state/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 
 function App() {
+	const [count, setCount] = useState(0);
+
+	const decrementCount = () => {
+		setCount(count - 1);
+	};
+
+	const incrementCount = () => {
+		setCount(count + 1);
+	};
+
 	return (
 		<>
-			<button>-</button>
-			<span>0</span>
-			<button>+</button>
+			<button onClick={decrementCount}>-</button>
+			<span>{count}</span>
+			<button onClick={incrementCount}>+</button>
 		</>
 	);
 }

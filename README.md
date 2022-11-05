5bb0e56abf5d46175335c4802084fe8bee9dd736
pavelFaraday <devstudio.corp@gmail.com>
2022-11-05 20:21:59 +0400

    basic Class Component

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Small React Projects
 
 1. use-state - simple projects for understanding how works React **useState** Hook.
+2. class-components - What is Class Components
---
+++ class-components/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Welcome from "./components/Welcome";
+
+function App() {
+	return (
+		<>
+			<h1>App Component h1</h1>
+			<Welcome />
+		</>
+	);
+}
+
+export default App;

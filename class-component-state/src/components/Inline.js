72d7b4fae701890e5eb9fd88dfc5c4a2807c1cfd
pavelFaraday <devstudio.corp@gmail.com>
2022-11-24 18:24:08 +0400

    inline styling as object

--- class-component-state/src/components/Inline.js
+++ class-component-state/src/components/Inline.js
@@ -10,6 +10,9 @@ const Inline = (props) => {
 		<div>
 			<h1 style={heading}>Inline</h1>
 			{/* every child component with classnames have inline scope  */}
+			<p style={{ backgroundColor: "yellow" }}>
+				inline styling as object
+			</p>
 		</div>
 	);
 };
